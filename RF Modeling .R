af49646c825a5cd61f4ba4aabd129de1d3758089
jeremy-sanchez <32178183+jeremy-sanchez@users.noreply.github.com>
2019-07-23 14:25:12 -0400

    Code organized into different scripts by purpose.

--- RF Modeling .R
+++ RF Modeling .R
@@ -49,12 +49,12 @@ test.resp <- ifelse(is.na(test.resp), 0 , test.resp)
 
 #use select variables:
 
-train.pred %>% select(c(x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
+train.pred %>% select(c(X, x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
 
                         pageRank_End, centrality_Origin, centrality_End, length, close_to_shot)) -> train.pred
 
 
-test.pred %>% select(c(x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
+test.pred %>% select(c(X, x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
 
                        pageRank_End, centrality_Origin, centrality_End, length, close_to_shot)) -> test.pred
 
@@ -132,16 +132,16 @@ test_2.resp <- ifelse(is.na(test_2.resp), 0 , test_2.resp)
 which(test_2.resp <0)
 
 #Without centrality 
-train_2.pred %>% select(c(x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
+train_2.pred %>% select(c(X, x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
                         
                         pageRank_End, length, close_to_shot)) -> train_2.pred
 
 
-test_2.pred %>% select(c(x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
+test_2.pred %>% select(c(X, x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
                        
                        pageRank_End, length, close_to_shot)) -> test_2.pred
 
-## Model 3 - NO NEGATIVES, ALL EXCEPT CENTRALITY ******
+## Model 3 - NO NEGATIVES, ALL EXCEPT CENTRALITY 
 
 rf.model3 <- randomForest(train_2.resp~., data = train_2.pred, importance = TRUE)
 rf.pred <- predict(rf.model3, newdata = test_2.pred)
@@ -159,29 +159,121 @@ test_2.pred %>% select(c(x.location, y.location, end.x.location,end.y.location, 
                          pageRank_End)) -> test_2.pred
 
 
-## Model 4 - location and PageRank
+## Model 4 - location and PageRank  *******
+set.seed(23451)
 rf.model4 <- randomForest(train_2.resp~., data = train_2.pred, importance = TRUE)
 rf.pred <- predict(rf.model4, newdata = test_2.pred)
-mse4 <- mean((test_2.resp-rf.pred)^2)
+mse4 <- mean((test_2.resp-rf.pred)^2) ; mse4
 
-mse4
 
 # -----------------------------------------------------------------------------
 # Post-model work 
 # -----------------------------------------------------------------------------
 
 #Variable importance
-rf.model3$importance
-varImpPlot(rf.model3) #close to shot the best
+rf.model4$importance
+varImpPlot(rf.model4) #close to shot the best
+
+rf.model4$
+
+#number of trees
+plot(rf.model4)
 
 #residuals vs fitted plot
-res.df <- data.frame(Actual = train_2.resp, Predicted = rf.model3$predicted)
+res.df <- data.frame(Actual = test_2.resp, Predicted = rf.model3$predicted, Close_To_Shot = test_2.pred$close_to_shot)
 
-ggplot(res.df, aes(x = Predicted, y = Actual)) + geom_point() + labs(title = "Actual vs Predicted xG",
+ggplot(res.df, aes(x = Predicted, y = Actual)) + geom_point(aes(col = Close_To_Shot)) + labs(title = "Actual vs Predicted xG",
                                                                      subtitle = "Random Forest")
 
-#do not have to run model3 again
-save(train_2, test_2, rf.model3, file = "RandForest.RData")
+#do not have to run model4 again
+save(train_2, test_2, rf.model4, file = "RandForest.RData")
+
+# ------------------------------------------------------------------------------------
+# Combine train and test into original dataset to prepare for adjusting by sequence.
+# ------------------------------------------------------------------------------------
+
+#redfine train_2 and test_2.pred to regain x Column and have only variables we used (use earlier code)
+
+#Remove negatives_____________________________________________________________________
+train_2 <- train
+test_2 <- test
+
+train_2$ExpectedGoal = ifelse(train_2$ExpectedGoal < 0, 0, train_2$ExpectedGoal)
+
+test_2$ExpectedGoal = ifelse(test_2$ExpectedGoal < 0, 0, test_2$ExpectedGoal)
+
+#_______________________________________________________
+
+#TRAIN
+train_2.resp <- train_2$ExpectedGoal
+
+train_2.pred <- train_2   #do not need to worry about removing ExpectedGoal, we want it in this case. 
+
+
+#TEST
+test_2.resp <- test_2$ExpectedGoal
+
+test_2.pred <- test_2 
+
+
+
+
+##WE change here to add X and keep only variables used in the analysis
+train_2.pred %>% select(c(X, match, type.name, possession_team.name, possession, x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
+                          
+                          pageRank_End, ExpectedGoal)) -> train_2.pred
+
+
+test_2.pred %>% select(c(X, match, type.name, possession_team.name, possession, x.location, y.location, end.x.location,end.y.location, pageRank_Origin,
+                         
+                         pageRank_End, ExpectedGoal)) -> test_2.pred
+
+
+#add adjusted column to predictors we chose for model 4
+train_2.pred$fittedxG <- rf.model4$predicted
+
+test_2.pred$fittedxG <- rf.pred
+  
+
+#merge train and test to create orignal dataset
+soccer <- rbind(train_2.pred,test_2.pred) %>% arrange(., X) -> soccer
+
+View(head(soccer, 40))
+
+
+# ------------------------------------------------------------------------------------
+# Use adjusting code from GAM Modeling to get xG per pass.
+# ------------------------------------------------------------------------------------
+
+#function to calculate xG per pass. 
+sum_fitted <- function(df) {
+  sumValues <- sum(df$fittedxG)
+  df$fittedxG <- sumValues
+  df$fittedxG
+}
+
+#filter just by passes and perform function.
+passes <- soccer %>% filter(., type.name == "Pass")
+
+
+passes %>% group_by(match, possession, ExpectedGoal) %>% nest() %>% mutate(summ = map(data, sum_fitted)) %>% unnest() -> passes
+View(passes)
+passes %>% mutate(adjusted = (fittedxG/summ)*ExpectedGoal) -> passes.adj
+View(passes.adj)
+
+
+# ------------------------------------------------------------------------------------
+# Write to final file.
+# ------------------------------------------------------------------------------------
+
+write_csv(passes.adj, "adjusted.csv")
+
+
+
+
+
+
+
 
 
 
